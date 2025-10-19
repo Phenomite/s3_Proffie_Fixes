234f1fbf3e3e1005eb934c08f81c2e7837657ffe
Phenomite <8285537+Phenomite@users.noreply.github.com>
2025-10-19 23:25:32 +1100

    preconfig5 font configs

---
+++ 000 SD CARD/README.md
@@ -0,0 +1,5 @@
+Folder contains folders:
+
+1) Fonts folders + [config/smoothsw].ini
+2) common
+3) tracks
--- CLONE CONFIGS TO FONT DIRS.bat
+++ CLONE CONFIGS TO FONT DIRS.bat
@@ -2,7 +2,7 @@ REM @echo off
 setlocal EnableDelayedExpansion
 set "configFile=config.ini"
 set "smoothswFile=smoothsw.ini"
-set "destinationDirectory=000 ALL FONTS"
+set "destinationDirectory=000 SD CARD"
 set "depthLevel=1"
 
 :CopyFilesAtDepth
